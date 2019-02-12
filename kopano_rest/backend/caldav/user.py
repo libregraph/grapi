b7212932aa967b9a60dd7126c458019e41822618
Mark Dufour <m.dufour@kopano.com>
2019-02-12 09:51:08 +0100

    rest: fill in caldav basics
    /me/events
    /me/events/id

---
+++ kopano_rest/backend/caldav/user.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import datetime
+import json
+
+from .resource import Resource
+from . import utils
+
+class UserResource(Resource):
+
+    def on_get(self, req, resp, userid=None, folderid=None, itemid=None, method=None):
+        if method == 'events': # TODO store-wide?
+            value = []
+
+            calendar = utils.calendar()
+            for event in calendar.date_search(datetime.datetime(2019, 1, 1), datetime.datetime(2020, 1, 1)):
+                value.append(utils.convert_event(event))
+
+            data = {'value': value}
+
+            resp.content_type = 'application/json'
+            resp.body = json.dumps(data, indent=2) # TODO stream
---
+++ kopano_rest/backend/caldav/utils.py
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+import base64
+import codecs
+import os
+
+import vobject
+import caldav
+
+HOST = 'https://webapp.kopano.com:8443'
+USER = os.getenv('GRAPI_USER')
+PASSWORD = os.getenv('GRAPI_PASSWORD')
+
+def calendar():
+    client = caldav.DAVClient(HOST, username=USER, password=PASSWORD)
+    principal = client.principal()
+    for calendar in principal.calendars():
+        if calendar.name == 'Calendar': # TODO better lookup
+            return calendar
+
+def convert_event(event):
+    vobj = vobject.readOne(event.data)
+    fmt = '%Y-%m-%dT%H:%M:%S'
+    return {
+        'id': codecs.decode(base64.urlsafe_b64encode(codecs.encode(str(event.url), 'utf-8')), 'ascii'), # TODO don't use full url..
+        'subject': vobj.vevent.summary.value,
+        'start': {
+            'timeZone': 'UTC', # TODO
+            'dateTime': vobj.vevent.dtstart.value.strftime(fmt),
+        },
+        'end': {
+            'timeZone': 'UTC', # TODO
+            'dateTime': vobj.vevent.dtend.value.strftime(fmt),
+        },
+    }
