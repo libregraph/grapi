840c087f8a49c57e02f2590d846060e23dfb681b
Jelle van der Waa <j.vanderwaa@kopano.com>
2019-03-15 11:14:07 +0100

    scripts: make backends configurable for devrunner
    As bonus, use argparse instead of a home-grew sys.argv parser. Also
    allow the backends to be configurable when running the devrunner.

--- scripts/devrunner.py
+++ scripts/devrunner.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: AGPL-3.0-or-later
+import argparse
 import multiprocessing
-import sys
 from wsgiref.simple_server import make_server
 
 from grapi.api.v1 import RestAPI
 from grapi.api.v1 import NotifyAPI
 
-REST_PORT = int(sys.argv[1]) if len(sys.argv) > 2 else 8000
-NOTIFY_PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 8001
+REST_PORT=8000
+NOTIFY_PORT=8001
+BACKEND = ['kopano']
+
+parser = argparse.ArgumentParser(description='grapi development runner')
+parser.add_argument('--rest-port', type=int, default=REST_PORT, help='the rest api port (default: {})'.format(REST_PORT))
+parser.add_argument('--notify-port', type=int, default=NOTIFY_PORT, help='the notify api port (default: {})'.format(NOTIFY_PORT))
+parser.add_argument('--backends', type=str,nargs='+', default=BACKEND, help='backends to enable (space-seperated) (default: {})'.format(BACKEND))
+args = parser.parse_args()
+
 
 def run_rest():
-    make_server('localhost', REST_PORT, RestAPI()).serve_forever()
+    make_server('localhost', args.rest_port, RestAPI(backends=args.backends)).serve_forever()
 
 def run_notify():
-    make_server('localhost', NOTIFY_PORT, NotifyAPI()).serve_forever()
+    make_server('localhost', args.notify_port, NotifyAPI(backends=args.backends)).serve_forever()
 
 multiprocessing.Process(target=run_rest).start()
 multiprocessing.Process(target=run_notify).start()
