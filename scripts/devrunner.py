590fd8d8d75819d96b6e51a999772faf13f3a633
Jelle van der Waa <j.vanderwaa@kopano.com>
2020-03-10 16:39:36 +0100

    Add missing space to devrunner.py in scripts
    Fix a PEP8 formatting violation in devrunner.py

--- scripts/devrunner.py
+++ scripts/devrunner.py
@@ -7,14 +7,14 @@ from wsgiref.simple_server import make_server
 from grapi.api.v1 import RestAPI
 from grapi.api.v1 import NotifyAPI
 
-REST_PORT=8000
-NOTIFY_PORT=8001
+REST_PORT = 8000
+NOTIFY_PORT = 8001
 BACKEND = ['kopano']
 
 parser = argparse.ArgumentParser(description='grapi development runner')
 parser.add_argument('--rest-port', type=int, default=REST_PORT, help='the rest api port (default: {})'.format(REST_PORT))
 parser.add_argument('--notify-port', type=int, default=NOTIFY_PORT, help='the notify api port (default: {})'.format(NOTIFY_PORT))
-parser.add_argument('--backends', type=str,nargs='+', default=BACKEND, help='backends to enable (space-seperated) (default: {})'.format(BACKEND))
+parser.add_argument('--backends', type=str, nargs='+', default=BACKEND, help='backends to enable (space-seperated) (default: {})'.format(BACKEND))
 args = parser.parse_args()
 
 
