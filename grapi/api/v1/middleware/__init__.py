4f27b7bea7ef792bdbccbc18a4d4c83b095b635c
Jelle van der Waa <j.vanderwaa@kopano.com>
2021-04-22 12:33:05 +0200

    Sort imports alphabetically

--- grapi/api/v1/middleware/__init__.py
+++ grapi/api/v1/middleware/__init__.py
@@ -1,6 +1,6 @@
 """Middlewares package."""
-from .request_id import RequestId
 from .request_body_extractor import RequestBodyExtractor
+from .request_id import RequestId
 from .resource_patcher import ResourcePatcher
 from .response_headers import ResponseHeaders
 
